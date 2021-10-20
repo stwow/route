2a8178ff8cca9920c1cb4f975b8c98b36129457e
andrei <stackkk@yandex.ru>
2021-10-20 13:49:39 +0300

    third commit

---
+++ cities/models.py
@@ -0,0 +1,14 @@
+
+from django.db import models
+
+
+class City(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name ="Город"
+        verbose_name_plural = 'Города'
+        ordering = ['name']
