511f8a29d33ba9b1ada04a4bcc52637c786c03a5
andrei <stackkk@yandex.ru>
2021-11-14 20:12:45 +0300

    2

--- cities/urls.py
+++ cities/urls.py
@@ -1,10 +1,14 @@
-from .views import cities, CityDetailView
+from .views import cities, CityDetailView,CityCreateView, CityUpdateView,CityDeleteView,CityView
 from django.urls import path
 
 app_name = 'cities'
 
 urlpatterns = [
-    path('', cities, name = 'cities'),
+    # path('', cities, name = 'cities'),
+    path('', CityView.as_view(), name = 'cities'),
     path('<int:pk>/', CityDetailView.as_view(), name = 'detail'),    #<int:pk> означает что тут целое число которое будет переведено в pk
+    path('add/', CityCreateView.as_view(), name ='create' ),
+    path('update/<int:pk>/',  CityUpdateView.as_view(), name = 'update'),
+    path('delete/<int:pk>/',  CityDeleteView.as_view(), name = 'delete'),
 ]
 
--- cities/views.py
+++ cities/views.py
@@ -1,22 +1,71 @@
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
-
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView, ListView
 from .models import City
 from django.views import View
-
-
+from .forms import CityForm
 
 
 def cities(request, pk =None):
-    if pk:
-        city = get_object_or_404(City, id = pk)
-        context = {'object': city}
-        return render(request, 'cities/detail.html', context)
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        if form.is_valid():
+            form.save()
+    # if pk:
+    #     city = get_object_or_404(City, id = pk)
+    #     context = {'object': city}
+    #     return render(request, 'cities/detail.html', context)
+    form = CityForm()
     qs = City.objects.all()
-    context = {'objects_list': qs}
+    lst = Paginator(qs, 5)
+    page_number = request.GET.get('page')
+    page_obj = lst.get_page(page_number)
+    context = {'page_obj': page_obj, 'form': form}
     return render(request, 'cities/home.html', context)
 
 
 class CityDetailView(DetailView):
     queryset = City.objects.all()
     template_name = 'cities/detail.html'
+
+
+class CityCreateView(SuccessMessageMixin, CreateView):
+    model = City
+    form_class = CityForm
+    template_name = 'cities/create.html'
+    success_url = reverse_lazy('cities:cities')
+    success_message = "Город успешно создан"
+
+
+class CityUpdateView(SuccessMessageMixin, UpdateView):
+    model = City
+    form_class = CityForm
+    template_name = 'cities/update.html'
+    success_url = reverse_lazy('cities:cities')
+    success_message = "Город успешно отредактирован"
+
+
+class CityDeleteView(DeleteView):
+    model = City
+   # template_name = 'cities/delete.html'
+    success_url = reverse_lazy('cities:cities')
+
+    def get(self, request, *args, **kwargs):   #удаление - без перехода на форму (т.е. без подтверждения. только по урлу)
+        messages.success(request,'Город успешно удалён')
+        return self.post(request, *args, **kwargs)
+
+
+class CityView(ListView):
+    paginate_by = 3
+    model = City
+    template_name = 'cities/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        form = CityForm()
+        context['form'] = form
+        return context
+
